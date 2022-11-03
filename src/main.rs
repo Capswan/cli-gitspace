882f0732f346a0beddfc5b05552a8f830644a729
alechp <me@alechp.com>
2022-11-02 19:56:49 -0700

    added clap dep, initialized with init and sync subcommands

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,30 @@
+use clap::{Parser, Subcommand};
+use std::path::PathBuf;
+
+#[derive(Debug, Parser)]
+#[clap(author, version, about)]
+struct Arguments {
+    #[clap(short, long)]
+    config_file: Option<String>,
+
+    #[clap(subcommand)]
+    cmd: SubCommand,
+}
+
+#[derive(Subcommand, Debug)]
+enum SubCommand {
+    Init {},
+    Sync {},
+}
+
 fn main() {
-    let args: Vec<String> = std::env::args().collect();
-    dbg!(args);
+    let args = Arguments::parse();
+    match args.cmd {
+        SubCommand::Init {} => {
+            println!("init");
+        }
+        SubCommand::Sync {} => {
+            println!("sync");
+        }
+    }
 }
